9201d5d6819a244683af3889d62894985a706da3
lakshmin81 <lakshmi.n@smytten.com>
2026-06-22 23:16:21 +0530

    removed data base key

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 const connectDb= async() => {
-    await mongoose.connect("mongodb+srv://nlprasanna52_db_user:zojVUA2fLRX5NGzO@node-season1.r3hkwum.mongodb.net/devTinder")
+    
 }
 
 
