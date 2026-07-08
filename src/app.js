336468ddff8c23b279039ce5ec7adfd6aac73dc8
lakshmin81 <lakshmi.n@smytten.com>
2026-07-08 23:30:21 +0530

    made changes for the deployment process

--- src/app.js
+++ src/app.js
@@ -6,15 +6,31 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://dev-communications-web.vercel.app",
+];
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  }),
+  })
 );
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
